f742c4c7db65d22e880e4565857448c86c33db25
Aaditya <aadityajujagar@gmail.com>
2024-12-19 10:18:39 +0530

    ready to create the shop section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import { Home } from "./pages/home/Home";
 import { About } from "./pages/about/About";
-import { Products } from "./pages/products/products";
+import { Products } from "./pages/products/Products";
 import { Contact } from "./pages/contact/Contact";
 import { Cart } from "./pages/cart/Cart";
 import { Footer } from "./components/footer/Footer";
